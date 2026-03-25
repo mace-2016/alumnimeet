d7e0d9ef92bfe6f02214aeb6613f26905898effa
mace-2016 <mace2016alumni@gmail.com>
2026-03-25 22:50:32 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,11 +81,11 @@ export default function RootLayout({
                     </div>
                      <div className="flex items-center gap-3 px-5 py-3 text-[10px] font-bold uppercase tracking-[0.2em] text-slate-400 bg-slate-50/50 cursor-not-allowed">
                       <span className="w-1.5 h-1.5 rounded-full bg-slate-300"></span>
-                      03:███ ██████ <Lock className="w-3 h-3 opacity-50" />
+                      03:█████ ████ <Lock className="w-3 h-3 opacity-50" />
                     </div>
                      <div className="flex items-center gap-3 px-5 py-3 text-[10px] font-bold uppercase tracking-[0.2em] text-slate-400 bg-slate-50/50 cursor-not-allowed">
                       <span className="w-1.5 h-1.5 rounded-full bg-slate-300"></span>
-                      04:█████████ ████ <Lock className="w-3 h-3 opacity-50" />
+                      04:██ █ █████ <Lock className="w-3 h-3 opacity-50" />
                     </div>
                   </div>
                 </div>
